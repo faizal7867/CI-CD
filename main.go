533836f72d8ae7c18eb404f8d8fb8643eb932dc9
faizal7867 <faizalmohamed910@gmail.com>
2024-12-14 17:57:10 +0530

    fff

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, AWS EC2  yesssssssss!")
+	fmt.Fprintln(w, "Hello, AWS EC2 Yes faizal!")
 }
 
 func main() {
